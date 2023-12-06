da96c323a10ec5b187b1c2530a2dcd5ea6caaf25
Neill Erasmus <neill.erasmus20@gmail.com>
2023-12-06 09:13:31 +0200

    Updated main.

--- main.py
+++ main.py
@@ -28,7 +28,21 @@ def IsObstacleDetected(screen_image, start_x : int, end_x : int, search_y : int,
     return False
 
 def main() -> None:
-    pass
+    top, left, screen_width, screen_height = GetGameScreenDimension()
+    last_update_second = 0
+    total_time_elapsed = 0
+    search_y, search_x_start, search_x_end = 350, 435, 450
+    search_y2 = 275
+    while True:
+        start_time = time.time()
+        if keyboard.is_pressed("q"):
+            break
+        search_x_end, last_update_second = UpdateSearchAreaEnd(search_x_end, screen_width, last_update_second, total_time_elapsed)
+        game_screen = pyautogui.screenshot(region=(left, top, screen_width, screen_height))
+        if IsObstacleDetected(game_screen, search_x_start, search_x_end, search_y, search_y2):
+            keyboard.press(" ")
+        elapsed_time = time.time() - start_time
+        total_time_elapsed += elapsed_time
 
 if __name__ == "__main__":
     main()
