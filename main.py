5dab605cede2959c207c029b9edea82549a540b9
Neill Erasmus <neill.erasmus20@gmail.com>
2023-12-06 09:12:51 +0200

    Added UpdateSearchAreaEnd method.

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def GetPixelColour(image, x : int, y : int) -> Tuple[int, int, int]:
     pixel_data = image.load()
     return pixel_data[x, y]
 
+def UpdateSearchAreaEnd(x_end : int, screen_width : int, last_update : int, total_time_elapsed : float) -> Tuple[int, int]:
+    if math.floor(total_time_elapsed) != last_update:
+        x_end += 4
+        if x_end >= screen_width:
+            x_end = screen_width
+        last_update = math.floor(total_time_elapsed)
+    return x_end, last_update
+
 def main() -> None:
     pass
 
