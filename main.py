4ad80e250f41e27751db9f02de23d49cbee0bd4f
Neill Erasmus <neill.erasmus20@gmail.com>
2023-12-06 09:12:25 +0200

    Added get methods.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import keyboard
 import time
 import math
 
+def GetGameScreenDimension() -> Tuple[int, int, int, int]:
+    top, left, width, height = 293, 0, 1920, 465
+    return top, left, width, height
+
+def GetPixelColour(image, x : int, y : int) -> Tuple[int, int, int]:
+    pixel_data = image.load()
+    return pixel_data[x, y]
+
 def main() -> None:
     pass
 
