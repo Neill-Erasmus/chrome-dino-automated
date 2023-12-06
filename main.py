7b5c8ab9acb9e2c3d047791b3428621c958ec758
Neill Erasmus <neill.erasmus20@gmail.com>
2023-12-06 09:13:11 +0200

    Added IsObstacleDetected method.

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ def UpdateSearchAreaEnd(x_end : int, screen_width : int, last_update : int, tota
         last_update = math.floor(total_time_elapsed)
     return x_end, last_update
 
+def IsObstacleDetected(screen_image, start_x : int, end_x : int, search_y : int, search_y2 : int) -> bool:
+    background_color = GetPixelColour(screen_image, 440, 30)
+    for x_pos in reversed(range(start_x, end_x)):
+        if (GetPixelColour(screen_image, x_pos, search_y) != background_color or GetPixelColour(screen_image, x_pos, search_y2) != background_color):
+            return True
+    return False
+
 def main() -> None:
     pass
 
