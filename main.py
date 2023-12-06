6829b4b31ac94258fb2d5903dcf493c25b583abd
Neill Erasmus <neill.erasmus20@gmail.com>
2023-12-06 09:36:15 +0200

    Added documentation.

--- main.py
+++ main.py
@@ -5,14 +5,52 @@ import time
 import math
 
 def GetGameScreenDimension() -> Tuple[int, int, int, int]:
+    """
+    GetGameScreenDimension Function
+
+    Returns the dimensions of the game screen.
+
+    Returns:
+    Tuple[int, int, int, int]: Dimensions (top, left, width, height) of the game screen.
+    """
+    
     top, left, width, height = 293, 0, 1920, 465
     return top, left, width, height
 
 def GetPixelColour(image, x : int, y : int) -> Tuple[int, int, int]:
+    """
+    GetPixelColour Function
+
+    Gets the color of a pixel at a given position in an image.
+
+    Parameters:
+    - image: The image to get the pixel color from.
+    - x: X-coordinate of the pixel.
+    - y: Y-coordinate of the pixel.
+
+    Returns:
+    Tuple[int, int, int]: RGB values of the pixel color.
+    """
+    
     pixel_data = image.load()
     return pixel_data[x, y]
 
 def UpdateSearchAreaEnd(x_end : int, screen_width : int, last_update : int, total_time_elapsed : float) -> Tuple[int, int]:
+    """
+    UpdateSearchAreaEnd Function
+
+    Updates the end position of the search area based on time.
+
+    Parameters:
+    - x_end: Current end position of the search area.
+    - screen_width: Width of the game screen.
+    - last_update: Last time the search area was updated.
+    - total_time_elapsed: Total time elapsed since the script started.
+
+    Returns:
+    Tuple[int, int]: Updated end position and the last time updated.
+    """
+    
     if math.floor(total_time_elapsed) != last_update:
         x_end += 4
         if x_end >= screen_width:
@@ -21,6 +59,22 @@ def UpdateSearchAreaEnd(x_end : int, screen_width : int, last_update : int, tota
     return x_end, last_update
 
 def IsObstacleDetected(screen_image, start_x : int, end_x : int, search_y : int, search_y2 : int) -> bool:
+    """
+    IsObstacleDetected Function
+
+    Checks if obstacles are detected in the specified region.
+
+    Parameters:
+    - screen_image: The image of the game screen.
+    - start_x: Starting X-coordinate of the search area.
+    - end_x: Ending X-coordinate of the search area.
+    - search_y: Y-coordinate of the search area.
+    - search_y2: Additional Y-coordinate for a secondary search area.
+
+    Returns:
+    bool: True if an obstacle is detected, False otherwise.
+    """
+    
     background_color = GetPixelColour(screen_image, 440, 30)
     for x_pos in reversed(range(start_x, end_x)):
         if (GetPixelColour(screen_image, x_pos, search_y) != background_color or GetPixelColour(screen_image, x_pos, search_y2) != background_color):
